a386f9b2b97566a0f4a60a9878545fe79fa469f7
Sicelo Sitsha <70627871+sicelo17@users.noreply.github.com>
2024-08-08 02:03:38 +0200

    add sales summary

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,13 @@
 
 import CardPopularProducts from "./CardPopularProducts";
 import CardPurchaseSummary from "./CardPurchaseSummary";
+import CardSalesSummary from "./CardSalesSummary";
 
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
-      <div className="row-span-3 xl:row-span-6 bg-gray-500"></div>
+      <CardSalesSummary />
       <CardPurchaseSummary />
       <div className="row-span-3 bg-gray-500"></div>
       <div className="md:row-span-3 xl:row-span-2 bg-gray-500"></div>
