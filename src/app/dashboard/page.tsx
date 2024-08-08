96380680815b0d742073021cf483087ec1d8bd6a
Sicelo Sitsha <70627871+sicelo17@users.noreply.github.com>
2024-08-08 02:00:33 +0200

    add purchase summary

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import CardPopularProducts from "./CardPopularProducts";
+import CardPurchaseSummary from "./CardPurchaseSummary";
 
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
       <div className="row-span-3 xl:row-span-6 bg-gray-500"></div>
-      <div className="row-span-2 xl:row-span-3 md:col-span-2 xl:col-span-1 bg-gray-500"></div>
+      <CardPurchaseSummary />
       <div className="row-span-3 bg-gray-500"></div>
       <div className="md:row-span-3 xl:row-span-2 bg-gray-500"></div>
       <div className="md:row-span-3 xl:row-span-2 bg-gray-500"></div>
